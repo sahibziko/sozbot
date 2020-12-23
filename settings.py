d3f7d2c8a41aaf6e15d9a752007ad7a4499b7867
sahibziko <32236360+sahibziko@users.noreply.github.com>
2020-12-24 00:11:56 +0400

    Kategoriya ela vesi

--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@
 TOKEN = "1495875069:AAF9TSdEIwHxRhozbXf44Vjq_Ib1jHlqvFY"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
+with open('wordsone.txt', 'r', encoding='utf-8') as file:
+    word_listone = file.read().splitlines()
